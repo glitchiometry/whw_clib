8900926a0043f40a2835580969c1bb56c7339c17
Henry Wilkin <25754214+glitchlichkeit@users.noreply.github.com>
2022-08-16 00:36:23 -0400

    Completed preliminary 'numerical.c' specification

---
+++ aux/numerical_fubini_case_by_thickness.h
@@ -0,0 +1,22 @@
+// RESUME: add subcases, complete the following routines.
+double fubini_slice_bdry_case1()
+{
+	// RESUME: incorporate vertical reflection operation to reduce number of cases from 3 to 2.
+}
+
+double fubini_slice_bdry_case2()
+{
+	// RESUME: look for a way to incorporate vertical reflection symmetry to reduce cases further.
+}
+
+double fubini_slice_bdry_case3()
+{
+	// RESUME: refer to case 2, with horizontal reflection (and appropriate choice of left/right upper/lower limits.)
+}
+
+double fubini_slice_bdry_case4()
+{
+	// RESUME: look for a way to incorporate horizontal reflection symmetry as well as vertical reflection symmetry.
+}
+
+
